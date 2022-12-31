b6b6bf532060dfc78ac0fe4493823b313ac03009
ANACACHO <ana.f.alves@icloud.com>
2022-12-31 03:38:53 +0000

    Login e registo feito

--- src/service/oneTimePassword.js
+++ src/service/oneTimePassword.js
@@ -1,50 +1,45 @@
-const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
-const enviarCodigoParaEmail = async (emailUtilizador, codigoConfirmacao) => {
-  if(!emailUtilizador || !codigoConfirmacao) return 'Verifique o teu correio eletrónico!';
+let tempOTP;
 
-  /* Gerar código de confirmação. */
-
-  const randomNumber = crypto.randomBytes(3).toString('hex');
-  const OTP = randomNumber.padStart(6, '0');
-
-  // Armazena a OTP em uma variável temporária
-  let tempOTP = OTP;
-
-    // Cria um novo transporter usando o SMTP do Gmail
-    const transporter = nodemailer.createTransport({
-      host: 'mail.cursar.pt',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'geral@cursar.pt',
-        pass: 'Cur$@r2022'
-      }
-    });
+const emailEnviado = async (emailUtilizador) => {
   
-    // Define os detalhes do email xbpelxeibydzvafd
-    const mailOptions = {
-      from: '"Cursar" <geral@cursar.pt>',
-      to: emailUtilizador,
-      subject: 'Código de autenticação',
-      text: `Olá, o seu código de autenticação é: ${tempOTP} \nEste código tem duração de 1 minuto, após o limite de tempo expirar, o código tornar-se-à inválido, o que vai implicar a reintrodução do endereço de e-mail.\n\nCursar`
-    };
-  
-  // Enviar o email
-  await transporter.sendMail(mailOptions);
+  const randomOTP = Math.round(Math.random() * 10000).toString(10).padEnd(6, '0'); // Gera número aleatório no intervalo de 0 a 10000
+  tempOTP = randomOTP; //Armazena o valor aleatório na variável temporária.
 
-  // Depois de 30 segundos, elimina o valor da variável temporária
+  // Cria um novo transporter usando o SMTP do Gmail.
+  const transporter = nodemailer.createTransport({
+    host: 'mail.cursar.pt',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'geral@cursar.pt',
+      pass: 'Cur$@r2022'
+    }
+  });
+  
+  // Define os detalhes do email.
+  const mailOptions = {
+    from: '"Cursar" <geral@cursar.pt>',
+    to: emailUtilizador,
+    subject: 'Código de autenticação',
+    text: `Olá, o seu código de autenticação é: ${tempOTP} \nEste código tem duração de 1 minuto, após o limite de tempo expirar, o código tornar-se-à inválido, o que vai implicar a reintrodução do endereço de e-mail.\n\nCursar`
+  };
+  
+  await transporter.sendMail(mailOptions); // Enviar e-mail.
+
+  // Depois de 1 minuto, elimina o valor da variável temporária.
   setTimeout(() => {
     tempOTP = null;
-  }, 60000);
+  }, 60000); 
 
-  if(codigoConfirmacao === tempOTP){
-    return true;
-  } 
-  
-  return false;
-
+  return tempOTP;
 }
 
-module.exports = {enviarCodigoParaEmail};
+// Compara o número o número gerado com o código enviado pelo utilizador e retorna um booleano.
+const compareCodigo = (codigoConfirmacao) => {
+  if(parseInt(tempOTP) === parseInt(codigoConfirmacao)) return true;
+  return false;
+}
+  
+module.exports = {emailEnviado, compareCodigo};
